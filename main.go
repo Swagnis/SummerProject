d0728006a689996d09a1539e5e183cfbe0c4d7b2
Unknown <Just.me.yup.yoo@gmail.com>
2017-07-15 00:59:51 +0500

    Starting

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+// Computer class for Computer type
+type Computer struct {
+	login string
+	pass  string
+	ip    string
+	port  int
+}
 
 func main() {
-	fmt.Println("Hello people")
+	startArg := flag.Bool("start", false, "a boolean")    // Запуск
+	stopArg := flag.Bool("stop", false, "a boolean")      // Остановка
+	ipArg := flag.String("ip", "0.0.0.0", "a string")     // Задает Ip
+	conArg := flag.Bool("new", false, "a boolean")        // Создание нового подключения
+	loginArg := flag.String("login", "admin", "a string") // Логин для подключения
+	passArg := flag.String("pass", "", "a string")        // Пароль для подключения
+	hostArg := flag.String("host", "Default", "a string") // Хостнейм
+	portArg := flag.Int("port", 22, "an int")             // Порт SSH соединения
+
+	flag.Parse()
+
+	if *startArg == true {
+		startProgram()
+	}
+
+	if *stopArg == true {
+		stopProgram()
+	}
+
+}
+
+// старт программы
+func startProgram() {
+
+	fmt.Println("Программа запущена")
+}
+
+// остановка программы.
+func stopProgram() {
+	fmt.Println("Программа остановлена")
 }
